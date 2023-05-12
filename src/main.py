31af72462f2b98f93376a73c01277b04e48867d7
cbasguti <sebastian.gutierrezj@udea.edu.co>
2023-05-12 14:54:32 +0200

    Add: Queso Parmesano 250g > Lacteos > 3

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from scrape import scrapeCarulla, scrapeEuro, scrapeExito, scrapeJumbo
 scrapeExito('https://www.exito.com/leche-entera-en-bolsa-x-1-litro-125079/p', 'Lacteos', '0')
 scrapeExito('https://www.exito.com/mantequilla-c-sal-barra-496847/p', 'Lacteos', '1')
 scrapeExito('https://www.exito.com/queso-crema-x-400g-101497/p', 'Lacteos', '2')
+scrapeExito('https://www.exito.com/queso-parmesano-x-250g-61417/p', 'Lacteos', '3')
 
 # Ejecuta las funciones de scraping de la tienda Euro
 
@@ -13,6 +14,8 @@ scrapeExito('https://www.exito.com/queso-crema-x-400g-101497/p', 'Lacteos', '2')
 scrapeEuro('https://www.eurosupermercados.com.co/leche-colanta-entera-1000ml.html', 'Lacteos','0')
 scrapeEuro('https://www.eurosupermercados.com.co/mantequilla-c-sal-250gr-colanta.html', 'Lacteos', '1')
 scrapeEuro('https://www.eurosupermercados.com.co/queso-crema-colanta-400gr.html', 'Lacteos', '2')
+scrapeEuro('https://www.eurosupermercados.com.co/queso-parmesano-rallado-x-250gr-alpina.html', 'Lacteos', '3')
+
 
 
 # Ejecuta las funciones de scraping de la tienda Carulla
@@ -21,6 +24,7 @@ scrapeEuro('https://www.eurosupermercados.com.co/queso-crema-colanta-400gr.html'
 scrapeCarulla('https://www.carulla.com/leche-entera-en-bolsa-x-1-litro-125079/p', 'Lacteos', '0')
 scrapeCarulla('https://www.carulla.com/mantequilla-sin-sal-barra-x-250-gr-61169/p', 'Lacteos', '1')
 scrapeCarulla('https://www.carulla.com/queso-crema-x-400g-101497/p', 'Lacteos', '2')
+scrapeCarulla('https://www.carulla.com/queso-parmesano-x-250g-61417/p', 'Lacteos', '3')
 
 # Ejecuta las funciones de scraping de la tienda Jumbo
 
@@ -28,3 +32,4 @@ scrapeCarulla('https://www.carulla.com/queso-crema-x-400g-101497/p', 'Lacteos', 
 scrapeJumbo('https://www.tiendasjumbo.co/leche-entera-liquida-x-1000cm3-colanta/p', 'Lacteos','0')
 scrapeJumbo('https://www.tiendasjumbo.co/mantequilla-con-sal-x-250-g-colanta/p', 'Lacteos','1')
 scrapeJumbo('https://www.tiendasjumbo.co/queso-crema-colanta-x-400g/p', 'Lacteos','2')
+scrapeJumbo('https://www.tiendasjumbo.co/queso-parmesano-bolsa-x-250g-alpina/p', 'Lacteos','3')
